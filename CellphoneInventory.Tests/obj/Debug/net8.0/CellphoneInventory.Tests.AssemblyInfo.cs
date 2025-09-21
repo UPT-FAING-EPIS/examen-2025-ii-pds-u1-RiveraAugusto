dbdc87eb51cccf1af82c22c2dc531cb913b09540
RiveraAugusto <ar2022073505@virtual.upt.pe>
2025-09-21 02:13:22 -0500

    Restaurado al commit 'Aplicacion desplegada' y corregido

--- CellphoneInventory.Tests/obj/Debug/net8.0/CellphoneInventory.Tests.AssemblyInfo.cs
+++ CellphoneInventory.Tests/obj/Debug/net8.0/CellphoneInventory.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CellphoneInventory.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9112255fccbc14a5ff997bac24ced73451484d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a483a2d9fac82dde24ba4cd7b455b3a70a5e2bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CellphoneInventory.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CellphoneInventory.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
